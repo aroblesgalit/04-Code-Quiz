918666a1c87da56a4e1211acdfd9439b1cdb7fb0
Alvin Galit <aroblesgalit@gmail.com>
2020-03-21 17:52:25 -0500

    Created logic for timer

--- script.js
+++ script.js
@@ -3,14 +3,52 @@ var highscoresButton = document.getElementById("highscoresButton");
 var highscoresScreen = document.getElementById("highscores");
 var branding = document.getElementById("branding");
 var homeScreen = document.getElementById("home");
+var quizUI = document.getElementById("quiz");
+var startButton = document.getElementById("startButton");
+var backButton = document.getElementById("backButton");
+var time = document.getElementById("time");
 
 
-// Logic for when HIGHSCORES button is clicked
+// Logic for when things are clicked
 highscoresButton.addEventListener("click", viewHighscores);
-
-
-// Logic for when Color Quiz branding is clicked
 branding.addEventListener("click", viewHomeScreen);
+startButton.addEventListener("click", startQuiz);
+backButton.addEventListener("click", viewHomeScreen);
+
+
+// Start quiz function
+function startQuiz() {
+    // View Quiz UI contents
+    quizUI.style.display = "block";
+    // Hide highscores button
+    hideHighscoresButton();
+    // View branding 
+    viewBranding();
+    // Hide home screen content
+    hideHomeScreen();
+    // Start timer
+    startTimer();
+}
+
+
+// Timer logic
+function startTimer() {
+    var timeLeft = parseInt(time.textContent);
+    var interval = setInterval(function() {
+       
+        timeLeft--;
+        if (timeLeft >= 10) {
+            time.textContent = timeLeft;
+        } else {
+            time.textContent = "0" + timeLeft;
+        }
+        
+        if (timeLeft === 0) {
+            clearInterval(interval)
+        }
+
+    }, 1000);
+}
 
 
 // Functions for hiding contents
@@ -22,20 +60,34 @@ function hideHighscores() {
     highscoresScreen.style.display = "none";
 }
 
+function hideQuizUI() {
+    quizUI.style.display = "none";
+}
+
 
 // Functions for viewing contents
 function viewHighscores() {
+    // Display Highscores
     highscoresScreen.style.display = "flex";
+    // Hide HIGHSCORES button
     hideHighscoresButton();
+    // View Branding
     viewBranding();
+    // Hide Home contents
     hideHomeScreen();
 }
 
 function viewHomeScreen() {
+    // Display Home contents
     homeScreen.style.display = "block";
+    // Hide Highscores contents
     hideHighscores();
+    // Hide branding
     hideBranding();
+    // View HIGHSCORES button
     viewHighscoresButton();
+    // Hide Quiz UI contents
+    hideQuizUI();
 }
 
 
